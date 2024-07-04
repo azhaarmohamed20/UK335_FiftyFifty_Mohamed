a387e2507e9a420aa3ecf71909deffadf874942a
Azhaar Mohamed <azhaar.mohamed20@gmail.com>
2024-07-04 13:32:42 +0200

    Install Expo vector icons and add to tab bar

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -1,15 +1,19 @@
 import { Tabs } from "expo-router";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { FontAwesome6 } from '@expo/vector-icons';
 
 export default function TabsLayout(){
     return (
         <Tabs>
             <Tabs.Screen name="index" options={{
                 headerTitle: "Home",
-                title: "Home"
+                title: "Home",
+                tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
             }} />
             <Tabs.Screen name="history" options={{
                 headerTitle: "History",
-                title: "History"
+                title: "History",
+                tabBarIcon: ({ color }) => <FontAwesome6 size={28} name="clock-rotate-left" color={color} />,
             }}/>
         </Tabs>
     )
