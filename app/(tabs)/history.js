d21e7d961875b28dbf575ec719f46ccf6e719ab2
Azhaar Mohamed <azhaar.mohamed20@gmail.com>
2024-07-05 09:05:12 +0200

    Fix styling issue where are only shown half

--- app/(tabs)/history.js
+++ app/(tabs)/history.js
@@ -78,7 +78,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     position: 'absolute',
-    height: '75%'
+    height: '68%'
   },
   title: {
     fontSize: 25,
